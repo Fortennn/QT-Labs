9ee2c861267be87169190235b69784cc49b53b28
Fortennn <fedotovvvvden@gmail.com>
2026-03-21 21:55:47 +0200

    HelpDesk v4

--- Helpdesk/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/cmake_install.cmake
+++ Helpdesk/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/fedot/Desktop/Helpdesk
+# Install script for directory: C:/Papki/qt-labs/Helpdesk
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
